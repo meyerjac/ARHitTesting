dd46eea69a55880f9186b74e8b51b0500acf65a6
Rayan Slim <rayanslim@Rayans-MBP.lan>
2017-08-15 02:39:39 +0300

    4

--- Whack a jellyfish/ViewController.swift
+++ Whack a jellyfish/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import ARKit
+import Each
 class ViewController: UIViewController {
-
+    var timer = Each(1).seconds
+    var countdown = 10
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var play: UIButton!
     @IBOutlet weak var SceneView: ARSCNView!
     let configuration = ARWorldTrackingSessionConfiguration()
     override func viewDidLoad() {
@@ -27,16 +31,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func play(_ sender: Any) {
+        self.setTimer()
         self.addNode()
+        self.play.isEnabled = false
     }
     
+    
     @IBAction func reset(_ sender: Any) {
+        self.timer.stop()
+        self.restoreTimer()
+        self.play.isEnabled = true
+        SceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.SceneView.session.pause()
+        self.SceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     func addNode() {
         let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
         let jellyfishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
-        jellyfishNode?.position = SCNVector3(0,0,-1)
+        jellyfishNode?.position = SCNVector3(randomNumbers(firstNum: -1, secondNum: 1),randomNumbers(firstNum: -0.5, secondNum: 0.5),randomNumbers(firstNum: -1, secondNum: 1))
         self.SceneView.scene.rootNode.addChildNode(jellyfishNode!)
     }
     @objc func handleTap(sender: UITapGestureRecognizer) {
@@ -46,10 +61,60 @@ class ViewController: UIViewController {
         if hitTest.isEmpty {
             print("didn't touch anything")
         } else {
+            if countdown > 0 {
             let results = hitTest.first!
-            let geometry = results.node.geometry
-            print(geometry)
+            let node = results.node
+            if node.animationKeys.isEmpty {
+                SCNTransaction.begin()
+                self.animateNode(node: node)
+                SCNTransaction.completionBlock = {
+                    node.removeFromParentNode()
+                    self.addNode()
+                    self.restoreTimer()
+                }
+                SCNTransaction.commit()
+                
+                }
+            }
         }
     }
+    
+    func animateNode(node: SCNNode) {
+        let spin = CABasicAnimation(keyPath: "position")
+        spin.fromValue = node.presentation.position
+        spin.toValue = SCNVector3(node.presentation.position.x - 0.2 ,node.presentation.position.y - 0.2 ,node.presentation.position.z - 0.2)
+        spin.duration = 0.07
+        spin.autoreverses = true
+        spin.repeatCount = 5
+        node.addAnimation(spin, forKey: "position")
+    }
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+
+    func setTimer() {
+        self.timer.perform { () -> NextStep in
+            self.countdown -= 1
+            self.timerLabel.text = String(self.countdown)
+            if self.countdown == 0 {
+                self.timerLabel.text = "you lose"
+                return .stop
+            }
+            return .continue
+        }
+    }
+    
+    func restoreTimer() {
+        self.countdown = 10
+        self.timerLabel.text = String(self.countdown)
+        
+    }
+    
+    
+    
+    
 }
+
+
 
